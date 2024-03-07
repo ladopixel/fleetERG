6becd5f5bdfc437db6e6c1da3796de348b5402c2
ladopixel <ladopixel@protonmail.com>
2024-03-07 19:12:09 +0100

    Show info token in sendTokens

--- src/components/SendTokens.tsx
+++ src/components/SendTokens.tsx
@@ -1,11 +1,14 @@
 import React, { useState, ChangeEvent } from 'react';
+import Token from '../components/Token';
 
 import { 
     Alert,
     AlertIcon,
+    Box,
     Button,
     FormControl,
     FormLabel,
+    Heading,
     Input ,
     Link,
     NumberInputField,
@@ -36,13 +39,28 @@ function SendTokens() {
     const [sent, setSent] = useState(false);
     const [tx, setTx] = useState('...');
 
+    const [tokenDetails, setTokenDetails] = useState({ id: '', name: '', description: '', decimals: 0, emissionAmount: 0, type: '', boxId: '' });
+    const [visible, setVisible] = useState(false)
 
     const handleTokenNameChange = (event: ChangeEvent<HTMLInputElement>) => {
         setWallet(event.target.value);
     }
 
-    const handleTokenIDChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setTokenID(event.target.value);
+    const handleTokenIDChange = async (event: ChangeEvent<HTMLInputElement>) => {
+        const tokenId = event.target.value;
+        setTokenID(tokenId);
+
+        try {
+            const response = await fetch(`https://api.ergoplatform.com/api/v1/tokens/${tokenId}`);
+            if (!response.ok) {
+                throw new Error('Error with API');
+            }
+            const data = await response.json();
+            setTokenDetails(data);
+            setVisible(true)
+        } catch (error) {
+            console.error('Error:', error);
+        }
     }
 
     const handleAmountChange = (valueAsString: string, valueAsNumber: number) => {
@@ -114,11 +132,24 @@ function SendTokens() {
                         </NumberInputStepper>
                     </NumberInput>
 
-                        
                     <Button colorScheme='teal' variant='outline' onClick={handleSubmit}> 
                         Send
                     </Button>
-                    
+
+                    {visible && (
+                        <Box textAlign={'left'}>
+                            <Heading size='md' mt={5} mb={3}>Token details</Heading>
+                            <Token 
+                                    id={tokenDetails.id}
+                                    name={tokenDetails.name}
+                                    description={tokenDetails.description}
+                                    decimals={tokenDetails.decimals}
+                                    emissionAmount={tokenDetails.emissionAmount}
+                                    type={tokenDetails.type}
+                                    boxId={tokenDetails.boxId}
+                                />
+                        </Box>
+                    )}
                     
                     {sent && (
                         <VStack>
